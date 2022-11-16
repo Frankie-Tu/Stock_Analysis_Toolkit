99a767a8097d04bc434d7331ffd24a7a5cc6c22c
Frankie Tu <hua.tu@hotmail.com>
2022-11-15 23:19:20 -0500

    fix yf_summary

--- scrappers/core/yf_summary.py
+++ scrappers/core/yf_summary.py
@@ -25,6 +25,22 @@ class YFSummary(ScrapperAbstract):
     def __init__(self, args, store_location, folder_name, file_save, logger=logging.getLogger("global")):
         super().__init__(tickers=args, store_location=store_location, folder_name=folder_name, file_save=file_save,logger=logger)
         self._dataframe = None
+    
+    def parse_rows(self, body: str):
+        columns = []
+        values = []
+        rows = body.find_all("tr")
+        cur_price = body.find(id="quote-header-info").find("fin-streamer").text
+
+        for row in rows:
+            col , val =  row.find_all("td")
+            columns.append(col.span.text)
+            values.append(val.text)
+        
+        columns.append("Current Price")
+        values.append(cur_price)
+        
+        return columns, values
 
     def data_parser(self, ticker):
         """
@@ -38,47 +54,24 @@ class YFSummary(ScrapperAbstract):
 
         try:
             self._logger.info("{}: Sending requests...".format(ticker))
-            my_soup = self.requester(url)
-            all_html = my_soup.find_all('table', {'class': "W(100%)"})
-            fifty2_week_range = list(filter(lambda x: x.td.span.text == "52 Week Range", all_html[0]
-                                            .find_all('tr', {'class': re.compile(r'^(Bxz\(bb\) Bdbw\(1px\) Bdbs\(s\) Bdc\(\$seperatorColor\) H\(36px\)){1}.*')})))
-            fifty2_week_range_value = fifty2_week_range[0].find_all('td', {'class': 'Ta(end) Fw(600) Lh(14px)'})[0].text.split(sep=' - ')
-            fifty2_week_range_low = fifty2_week_range_value[0]
-            fifty2_week_range_high = fifty2_week_range_value[1]
+            res = self.requester(url).body
 
-            # 1y Target Est
-            self._logger.info('{}: Scrapping 1 year target estimates'.format(ticker))
-            all_html = my_soup.find_all('table', {'class': "W(100%) M(0) Bdcl(c)"})
-            one_year_target = list(filter(lambda x: x.td.span.text == "1y Target Est", all_html[0].find_all('tr', {'class': re.compile(r'(Bxz\(bb\) Bdbw\(1px\) Bdbs\(s\) Bdc\(\$seperatorColor\) H\(36px\)){1}.*')})))[0]\
-                .find_all('td', {'class': 'Ta(end) Fw(600) Lh(14px)'})[0].span.text
-
-            # Stock price.
-            self._logger.info('{}: Scrapping price info & percentage change'.format(ticker))
-            all_html_2 = my_soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})
-
-            current_price = all_html_2[0].div.span.text
-
-            # Stock price change
-            change = all_html_2[0].div.find_all('span', {'class': re.compile(r"Trsdu\(0.3s\) Fw\(500\) Fz\(14px\)")})[0]\
-                .text.split(sep=' ')[1].replace('(', "").replace(')', "").replace('%', '')
-            change = str(float(change) / 100)
+            cols, vals = self.parse_rows(res)
 
         except IndexError:
             self._logger.exception("{}: returned html not in expected format".format(ticker))
             raise IndexError
 
-        fifty2_week_range_high, fifty2_week_range_low, current_price = list(map(lambda x: x.replace(',', ''), [fifty2_week_range_high, fifty2_week_range_low, current_price]))
+        for col, val in zip(cols, vals):
+            result_dict[col] = val
 
-        result_dict['52 Week Low'] = float(fifty2_week_range_low)
-        result_dict['52 Week High'] = float(fifty2_week_range_high)
+        result_dict['52 Week Low'] = float(result_dict['52 Week Range'].split(" - ")[0].replace(",", ""))
+        result_dict['52 Week High'] = float(result_dict['52 Week Range'].split(" - ")[1].replace(",", ""))
 
-        if one_year_target == 'N/A':
+        if result_dict["1y Target Est"] == 'N/A':
             result_dict['1y Target Est'] = 0
         else:
-            result_dict['1y Target Est'] = float(one_year_target.replace(',', ''))
-
-        result_dict['Change %'] = float(change)
-        result_dict['Current Price'] = float(current_price)
+            result_dict['1y Target Est'] = float(result_dict["1y Target Est"].replace(',', ''))
 
         # Based on  estimates provided by analysts and compare that to the current stock price
         self._logger.info('{}: Calculating growth potential and current price percentile'.format(ticker))
@@ -86,24 +79,16 @@ class YFSummary(ScrapperAbstract):
         result_dict['52 Week Percentile'] = (float(result_dict['Current Price']) - float(result_dict['52 Week Low'])) / \
                                             (float(result_dict['52 Week High']) - float(result_dict['52 Week Low']))
 
-        col = []
-        val = []
-
-        # Transfer data from result_dict to col and val list
-        for key, value in zip(result_dict.keys(), result_dict.values()):
-            col.append(key)
-            val.append(round(value, 4))
-
         # if self._dataframe already exists, append only
         self._lock.acquire()
         self._logger.debug("{} locked".format(ticker))
 
         if isinstance(None, type(self._dataframe)):
             self._logger.info('Creating dataframe...')
-            self._dataframe = pd.DataFrame(val, col, columns=[ticker])
+            self._dataframe = pd.DataFrame( result_dict.values(), result_dict.keys(), columns=[ticker])
         else:
             self._logger.info('Appending ' + ticker + ' to dataframe')
-            self._dataframe[ticker] = val
+            self._dataframe[ticker] = result_dict.values()
 
         self._lock.release()
         self._logger.debug("{} unlocked".format(ticker))
